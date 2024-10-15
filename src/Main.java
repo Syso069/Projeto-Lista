08f352db945c83be2b64b74fee695e2ae2da4c51
Syso069 <sisomd69@gmail.com>
2024-10-15 17:28:41 -0300

    adicionando menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hoje estou fazendo testes por aqui");
+        int opcao = 0;
+        Scanner numeroDigitado = new Scanner(System.in);
+        Menu mostraMenu = new Menu();
+
+        while (opcao != 5){
+            System.out.println("\nTO DO LIST\n" + mostraMenu.menu);
+            opcao = numeroDigitado.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Chama classe e/ou função criar lista");
+                    break;
+                case 2:
+                    System.out.println("Chama classe e/ou função editar lista");
+                    break;
+                case 3:
+                    System.out.println("Chama classe e/ou função visualizar lista");
+                    break;
+                case 4:
+                    System.out.println("Chama classe e/ou função excluir lista");
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu {
+        String menu = """
+            \n1- Criar lista.
+            2- Editar lista.
+            3- Ver lista.
+            4- Remover lista.
+            5- Sair.\n
+            """;
+}
