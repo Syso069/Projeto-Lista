7f6bf9bea1e6f9fa63a27603ded86cc98a8a16c9
Syso069 <sisomd69@gmail.com>
2024-10-18 20:52:32 -0300

    criando classe lista

---
+++ src/Lista.java
@@ -0,0 +1,21 @@
+public class Lista {
+    public void criaLista() {
+        System.out.println("Chamando função cria lista");
+    }
+
+    public  void editaLista() {
+        System.out.println("Chamando função edita lista");
+    }
+
+    public  void verLista() {
+        System.out.println("Chamando função ver lista");
+    }
+
+    public void excluirLista() {
+        System.out.println("Chamando função excluir lista");
+    }
+
+    public void saiDaLista() {
+        System.out.println("Chamando função saindo lista...");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int opcao = 0;
         Scanner numeroDigitado = new Scanner(System.in);
         Menu mostraMenu = new Menu();
+        Lista lista = new Lista();
 
         while (opcao != 5){
             System.out.println("\nTO DO LIST\n" + mostraMenu.menu);
@@ -12,22 +13,22 @@ public class Main {
 
             switch (opcao){
                 case 1:
-                    System.out.println("Chama classe e/ou função criar lista");
+                    lista.criaLista();
                     break;
                 case 2:
-                    System.out.println("Chama classe e/ou função editar lista");
+                    lista.editaLista();
                     break;
                 case 3:
-                    System.out.println("Chama classe e/ou função visualizar lista");
+                    lista.verLista();
                     break;
                 case 4:
-                    System.out.println("Chama classe e/ou função excluir lista");
+                    lista.excluirLista();
                     break;
                 case 5:
-                    System.out.println("Saindo...");
+                    lista.saiDaLista();
                     break;
                 default:
-                    System.out.println("Opção inválida!a");
+                    System.out.println("Opção inválida!");
             }
         }
     }
